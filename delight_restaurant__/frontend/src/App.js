cc2e29c1940874cda66fdb4ad18e182f4deac68e
sivakotireddy200288 <20028883@mydbs.ie>
2025-03-27 19:56:35 +0000

    Deleted src folder from public directory

---
+++ delight_restaurant__/frontend/src/App.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Home from './components/Home';
+import ReserveTable from './components/ReserveTable';
+import ContactUs from './components/ContactUs';
+import PlaceOrder from './components/PlaceOrder';
+import AdminDashboard from './components/AdminDashboard';
+import UserDashboard from './components/UserDashboard';
+import ManageMenu from './components/ManageMenu';
+import ManageReservations from './components/ManageReservations';
+import ManageOrders from './components/ManageOrders';
+import ManageFeedback from './components/ManageFeedback';
+import Register from './components/Register';
+import Login from './components/Login';
+import MenuPage from './components/MenuPage';
+import './App.css'; 

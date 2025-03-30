5911900a791869da55b26ffffc3db28ed787cc84
sivakotireddy200288 <20028883@mydbs.ie>
2025-03-30 15:13:42 +0100

    Creating and  updating Home.js

---
+++ delight_restaurant__/frontend/src/components/Home.js
@@ -0,0 +1,6 @@
+import React, { useState } from 'react';
+import Register from './Register';
+import Login from './Login';
+import Menu from './Menu';
+import ContactUs from './ContactUs';
+import './Home.css';

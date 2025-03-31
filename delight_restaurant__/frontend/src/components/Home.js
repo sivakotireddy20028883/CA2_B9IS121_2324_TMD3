4de93307bee84236b3f895e5ac159e9009145065
sivakotireddy200288 <20028883@mydbs.ie>
2025-03-31 20:47:17 +0100

    modified and  updating Home.js code

--- delight_restaurant__/frontend/src/components/Home.js
+++ delight_restaurant__/frontend/src/components/Home.js
@@ -4,3 +4,21 @@ import Login from './Login';
 import Menu from './Menu';
 import ContactUs from './ContactUs';
 import './Home.css';
+
+const Home = ({ onLogin }) => {
+    const [activeComponent, setActiveComponent] = useState(null);
+
+    const renderComponent = () => {
+        switch (activeComponent) {
+            case 'register':
+                return <Register />;
+            case 'login':
+                return <Login onLogin={onLogin} />;
+            case 'viewMenu':
+                return <Menu />;
+            case 'contactUs':
+                return <ContactUs />;
+            default:
+                return null;
+        }
+    };
